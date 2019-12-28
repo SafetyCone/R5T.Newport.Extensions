83945cc65c93b9eb8fac0cdc4b4b7dcff0672cb3
MinexAutomation <minex.automation@gmail.com>
2019-12-28 13:31:09 -0800

    Added static HttpClientHelper class with GetFromJsonAsync() method.

--- source/R5T.Newport.Extensions/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Newport.Extensions
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Newport.Extensions/Code/Net/Helpers/HttpClientHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using MagyarHttpClientHelper = R5T.Magyar.Net.HttpClientHelper;
+
+
+namespace R5T.Newport.Net
+{
+    public static class HttpClientHelper
+    {
+        public static async Task<T> GetFromJsonAsync<T>(string jsonUrlEndpoint)
+        {
+            var json = await MagyarHttpClientHelper.GetStringAsync(jsonUrlEndpoint);
+
+            var output = JsonConvert.DeserializeObject<T>(json);
+            return output;
+        }
+    }
+}
