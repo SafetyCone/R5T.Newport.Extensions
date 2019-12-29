7254fc561adca7f29d65672189d3f96910e28184
MinexAutomation <minex.automation@gmail.com>
2019-12-28 18:00:01 -0800

    Added Utilities.GetStandardJsonSerializer().

---
+++ source/R5T.Newport.Extensions/Code/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+
+namespace R5T.Newport
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Gets a <see cref="JsonSerializer"/> configured to produce indented JSON suitable for serialization to files.
+        /// </summary>
+        public static JsonSerializer GetStandardJsonSerializer()
+        {
+            var jsonSerializer = new JsonSerializer
+            {
+                Formatting = Formatting.Indented
+            };
+
+            return jsonSerializer;
+        }
+    }
+}
