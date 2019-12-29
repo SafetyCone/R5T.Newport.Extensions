731bfd16b2216f78ca9d877a3429c27b9ea6b35d
MinexAutomation <minex.automation@gmail.com>
2019-12-28 18:00:25 -0800

    Added Json serializer extensions.

---
+++ source/R5T.Newport.Extensions/Code/Extensions/JsonSerializerExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+using Newtonsoft.Json;
+
+using R5T.Magyar.IO;
+
+
+namespace R5T.Newport.Extensions
+{
+    public static class JsonSerializerExtensions
+    {
+        public static void Serialize<T>(this JsonSerializer jsonSerializer, string jsonFilePath, T value, bool overwrite = true)
+        {
+            using (var fileStream = FileStreamHelper.NewWrite(jsonFilePath, overwrite))
+            using (var streamWriter = new StreamWriter(fileStream))
+            {
+                jsonSerializer.Serialize(streamWriter, value);
+            }
+        }
+
+        public static T Deserialize<T>(this JsonSerializer jsonSerializer, string jsonFilePath)
+        {
+            using (var textReader = File.OpenText(jsonFilePath))
+            using (var jsonReader = new JsonTextReader(textReader))
+            {
+                var value = jsonSerializer.Deserialize<T>(jsonReader);
+                return value;
+            }
+        }
+    }
+}
