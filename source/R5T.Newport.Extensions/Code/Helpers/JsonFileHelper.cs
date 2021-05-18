c9585baec05224e8d70fbf5dc6a101521efa489e
David Coats <david@getsafetycone.com>
2021-05-17 22:13:12 -0700

    Add Json-related helpers.

---
+++ source/R5T.Newport.Extensions/Code/Helpers/JsonFileHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using R5T.Magyar.IO;
+
+using R5T.Newport.Extensions;
+
+
+namespace R5T.Newport
+{
+    public static class JsonFileHelper
+    {
+        /// <summary>
+        /// No async version since Newtonsoft does not have async!
+        /// </summary>
+        public static void WriteToFile<T>(string jsonFilePath, T value, bool overwrite = IOHelper.DefaultOverwriteValue)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+            };
+
+            var serializer = JsonSerializer.Create(settings);
+
+            serializer.Serialize(jsonFilePath, value, overwrite);
+        }
+    }
+}
---
+++ source/R5T.Newport.Extensions/Code/Helpers/JsonHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+
+namespace R5T.Newport
+{
+    public static class JsonHelper
+    {
+        public const Formatting DefaultFormatting = Formatting.Indented;
+
+
+        public static string ToJsonString<T>(T value, Formatting formatting = JsonHelper.DefaultFormatting)
+        {
+            var json = JsonConvert.SerializeObject(value, formatting);
+            return json;
+        }
+
+        public static T FromJsonstring<T>(string value)
+        {
+            var output = JsonConvert.DeserializeObject<T>(value);
+            return output;
+        }
+
+        /// <summary>
+        /// A quality-of-life name for <see cref="FromJsonstring{T}(string)"/>.
+        /// </summary>
+        public static T ToObject<T>(string value)
+        {
+            return JsonHelper.FromJsonstring<T>(value);
+        }
+    }
+}
